d1fe83295030f68cc48bdbccd5289ac07a1ac34d
flingyp <1213563369@qq.com>
2024-08-15 23:50:36 +0800

    chore:  开发 Header 部分中

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -14,7 +14,7 @@ export default async function Page(props: PageProps) {
   const localeDict = await getLocale(lang);
 
   return (
-    <main className="mt-[80px]">
+    <main className="pt-[80px]">
       <p>{localeDict["Lading Page Starter"]}</p>
       <p>{localeDict["description"]}</p>
 
